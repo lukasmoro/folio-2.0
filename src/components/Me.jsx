36341ef49d07d286a530dc2352b484e950f3a5d6
lukasmoro <97121073+lukasmoro@users.noreply.github.com>
2022-07-31 20:38:32 +0100

    forth commit

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -4,16 +4,27 @@ function Me() {
   return (
     <div className="container">
       <div className="text-block">
-        <h1>Lukas Moro</h1>
+        <h1>Hello, I am Lukas. 👋🏼</h1>
         <p>My passion is tinkering with {"<code/>"} and nodes.</p>
         <div className="line"></div>
         <p>
-          Always experimenting with interfaces for ambient and spatial computing
-          to help people focus on the things that matter.
+          I experiment with interfaces for ambient and spatial computing to help
+          people focus on the things that matter to them.
         </p>
         <p>
-          Currently working for <strong>Rolls-Royce Motorcars</strong> and
-          <strong> spatialLABS</strong>.
+          Currently working for{" "}
+          <a
+            href="https://www.rolls-roycemotorcars.com/en_GB/home.html"
+            className="link"
+          >
+            Rolls-Royce Motorcars
+          </a>{" "}
+          and
+          <a href="https://www.slabs.one" className="link">
+            {" "}
+            spatialLABS
+          </a>
+          .
         </p>
       </div>
     </div>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 import BigContainer from "./BigContainer";
+import ListItem from "./projectList.json";
+import "./Work.css";
 
 function Work() {
   return (
     <div className="container">
       <div className="text-block">
         <BigContainer />
-        <h1>Recent projects {"&"} explorations </h1>
+        {/* <h1>Projects {"&"} Explorations </h1> */}
         <p>
-          My most recent work and explorations, I use to extend my view and
-          learn tools that could be helpful for my work.
+          My most recent work and explorations, which I use to broaden my
+          horizon and learn tools that could be helpful for my work.
         </p>
         <div className="line"></div>
+        {ListItem.map((item) => {
+          return (
+            <ul className="list-item" key={item.id}>
+              <li className="item">{item.date}</li>
+              <a href="" className="link">
+                <li className="item">{item.name}</li>
+              </a>
+              {/* <li className="item">{item.sideproject}</li> */}
+            </ul>
+          );
+        })}
       </div>
     </div>
   );
