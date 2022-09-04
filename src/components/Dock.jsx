83997f1ef3ca7bdf8aa53e2ca588318566927c03
lukasmoro <97121073+lukasmoro@users.noreply.github.com>
2022-09-04 21:24:52 +0100

    minor changes

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -24,7 +24,7 @@ function Dock() {
         </BehaviourHover>
         <BehaviourHover rotation={20} timing={200}>
           <Link style={navStyle} to="/visual">
-            <li>drip</li>
+            <li>bild</li>
           </Link>
         </BehaviourHover>
         <BehaviourHover rotation={20} timing={200}>
