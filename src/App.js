cacb72e427e40336923a8eaf7ff670b00c0deff8
Lukas Moro <lukas.moro@me.com>
2024-03-04 14:40:20 +0100

    analytics

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Dock from './components/Dock';
 import AllRoutes from './components/AllRoutes';
 import './App.css';
+import { Analytics } from "@vercel/analytics/react"
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <div className="App" >
             <div id="wrapper">
               <AllRoutes />
+              <Analytics />
             </div>
             <Dock />
           </div>
