1ed1c130c016ffef6269ec97c770e5164858a018
Lukas Moro <lukas.moro@me.com>
2024-12-04 12:00:29 +0100

    new essay

--- src/components/Write.jsx
+++ src/components/Write.jsx
@@ -3,33 +3,55 @@ import DayDate from "./DayDate";
 import Thoughts from "./Data/listWriting.json";
 
 function Write() {
+  const renderTextWithSubheaders = (text) => {
+    return text.split("\n\n").map((block, index) => {
+      if (block.startsWith("##")) {
+        return (
+          <h3 key={index}>
+            {block.replace("##", "").trim()}
+          </h3>
+        );
+      } else {
+        return (
+          <p key={index}>
+            {block.split("\n").map((line, lineIndex, array) => (
+              <React.Fragment key={lineIndex}>
+                {line}
+                {lineIndex < array.length - 1 && <br />}
+              </React.Fragment>
+            ))}
+          </p>
+        );
+      }
+    });
+  };
 
   return (
-    
     <div className="container">
       <div className="text-block">
         <div>
-
           <DayDate />
 
           <h1>Essays</h1>
 
           <p className="spacer"></p>
 
-          <p>Writing as a way of making vague thoughts a bit more concrete in the process.<br />If you want to share thoughts on design or technology related matter, {" "}<a className="link" target="_blank" rel="noreferrer" href="mailto:lukas.moro@me.com">let's chat</a>.</p>
-          
-          {Thoughts.map((item) => {const paragraphs = item.text.split('\n').map((paragraph, index, array) => 
-          (
-            <React.Fragment key={index}>
-              {paragraph}
-              {index < array.length - 1 && <br />}
-            </React.Fragment>
-          ));
+          <p>
+            Writing as a way of making vague thoughts a bit more concrete in the process.
+            <br />
+            If you want to share thoughts on design or technology related matter,{" "}
+            <a
+              className="link"
+              target="_blank"
+              rel="noreferrer"
+              href="mailto:lukas.moro@me.com"
+            >
+              let's chat
+            </a>.
+          </p>
 
-          return (
-
+          {Thoughts.map((item) => (
             <div key={item.id}>
-
               <div className="line"></div>
 
               <ul className="thought-data">
@@ -39,20 +61,23 @@ function Write() {
 
               <h2>{item.header}</h2>
 
-              <p>{paragraphs}</p>
+              <div>{renderTextWithSubheaders(item.text)}</div>
 
               <div className="citation-container">
-                {Object.entries(item.resource).map(([key, value], index, array) => (
-                  <React.Fragment key={key}>
-                    <p className="citation" href={value} target="_blank" rel="noopener noreferrer">{value}</p>
-                    {index < array.length - 1 && <br />}
-                  </React.Fragment>
+                {Object.entries(item.resource).map(([key, value]) => (
+                  <p
+                    key={key}
+                    className="citation"
+                    href={value}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {value}
+                  </p>
                 ))}
               </div>
-
             </div>
-          );
-        })}
+          ))}
         </div>
         <div className="bottom-work"></div>
       </div>
